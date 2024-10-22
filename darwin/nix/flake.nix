c20d65c6427d2b59f99f9e653dfb024aa19b7248
Ryan Wittmers <rwittmers19@georgefox.edu>
2024-10-21 17:25:10 -0700

    Initial commit.

---
+++ darwin/nix/flake.nix
@@ -0,0 +1,98 @@
+{
+  description = "Zenful Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }:
+  let
+    configuration = { pkgs, config, ... }: {
+      nixpkgs.config.allowUnfree = true;
+
+      environment.systemPackages = [
+        pkgs.neovim
+        pkgs.mkalias
+        pkgs.micro
+        pkgs.vscode
+        pkgs.docker
+        pkgs.raycast
+      ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "mas"
+        ];
+        casks = [
+          "raycast"
+          "warp"
+          "firefox"
+          "arc"
+          "notion"
+          "notion-calendar"
+        ];
+
+        masApps = {
+          "Windows App" = 1295203466;
+        };
+
+        onActivation.cleanup = "zap";
+      };
+
+      services.nix-daemon.enable = true;
+
+      nix.settings.experimental-features = "nix-command flakes";
+
+      programs.zsh.enable = true;
+
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      system.stateVersion = 5;
+
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+        '';
+    };
+  in
+  {
+    darwinConfigurations."workMac" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration 
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Enable Homebrew via Nix
+            enable = true;
+
+            # User owning the Homebrew prefix
+            user = "ryan.wittmers";
+
+            # Automigrate Homebrew packages to Nix. Now that homebrew is migrated successfully, this option doesn't need to be in there.
+            # autoMigrate = true;
+          };
+        }
+      ];
+    };
+  };
+}
